4b2566b42726ddf8787b89d1d9bcadbfab2dd76d
Ardyezapratama <ardyezapratama@gmail.com>
2025-07-09 11:34:26 +0800

    Configuration

---
+++ config/contoh.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'author' => [
+        'first' => env('NAME_FIRST', 'Eza'),
+        'last' => env('NAME_LAST', 'Pratama')
+    ],
+    'email' => 'ardyezapratama@gmail.com'
+];
---
+++ tests/Feature/ConfigurationTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ConfigurationTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testConfig()
+    {
+        $firstName = config('contoh.author.first');
+        $lastName = config('contoh.author.last');
+        $email = config('contoh.email');
+
+        self::assertEquals('Eza', $firstName);
+        self::assertEquals('Pratama', $lastName);
+        self::assertEquals('ardyezapratama@gmail.com', $email);
+    }
+}
