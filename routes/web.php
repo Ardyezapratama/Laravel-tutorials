2b5453f364a68bdd211af2c34294157e142c92ff
Ardyezapratama <ardyezapratama@gmail.com>
2025-07-14 20:58:49 +0800

    Named Route

--- routes/web.php
+++ routes/web.php
@@ -47,21 +47,21 @@ Route::get('/hello-world', function () {
 // Route with parameter
 Route::get('products/{id}', function ($productId) {
     return "Product $productId";
-});
+})->name('product.detail');
 
 Route::get('/products/{product}/items/{item}', function ($productId, $itemId) {
     return "Product $productId, Item $itemId";
-});
+})->name('product.item.detail');
 
 // Route with parameter and regular expression
 Route::get('/categories/{id}', function ($categoryId) {
     return "Category $categoryId";
-})->where('id', '[0-9]+');
+})->where('id', '[0-9]+')->name('category.detail');
 
 // Optional Route Parameter
 Route::get('/users/{id?}', function (string $userId = '404') {
     return "User : $userId";
-});
+})->name('user.detail');
 
 // Route Conflict
 Route::get('/conflict/eza', function () {
@@ -70,4 +70,13 @@ Route::get('/conflict/eza', function () {
 
 Route::get('/conflict/{name}', function ($name) {
     return "Conflict $name";
+});
+
+Route::get('/produk/{id}', function ($id) {
+    $link = route('product.detail', ['id' => $id]);
+    return "Link $link";
+});
+
+Route::get('/produk-redirect/{id}', function ($id) {
+    return redirect()->route('product.detail', ['id' => $id]);
 });
