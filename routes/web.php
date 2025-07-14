f850402c53fc0b5ad32f2c54a756c3c5a863be2b
Ardyezapratama <ardyezapratama@gmail.com>
2025-07-14 16:06:50 +0800

    Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pzn', function () {
+    return "Hello Programmer Zaman Now";
+});
+
+// Redirect
+Route::redirect('/youtube', '/pzn');
+
+// Fallback for route not found
+Route::fallback(function () {
+    return "404 by Eza Pratama";
+});
